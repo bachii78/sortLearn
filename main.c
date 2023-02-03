cc6b0de4e4e1547f798ca2fac7e3d5f698dd7048
bachii78 <saat.aaaau78@gmail.com>
2023-02-03 23:09:05 +0300

    start

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<time.h>
+
+int main(){
+	int mas[1000];
+	srand(time(nullptr));
+	for(int i = 0; i < 1000; i++){
+		mas[i] = rand() % 200;
+	}
+	for(int i = 0; i < 500; i++){
+		std::cout << mas[i] << '\t' << mas[i + 1] << '\n';
+	}
+	return 0;
+}
+
+
