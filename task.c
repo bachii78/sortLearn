009b19d9c019aebf93cf2d89e4c622f70a0e79c1
bachii78 <saat.aaaau78@gmail.com>
2023-02-04 00:23:28 +0300

    заголовки

---
+++ task.c
@@ -0,0 +1,43 @@
+#include <task.h>
+
+void fillRandom(int*mass, int size){
+	srand(time(nullptr));
+        for (int i = 0; i < size; i++) {
+            *(mass + i) = random() % 100;
+        }
+}
+
+
+int showEl(const int* ar, int size, int N = 10, int M = 10){
+	for(n, n > 0, n--){
+		for(m, m > 0, m--){
+			std::cout << ar[10*(10 - n) + (10 - m)];
+		}
+	}
+
+
+}
+
+void swap(int&a, int&b){
+    int temp = a;
+    a = b;
+    b = temp;
+
+}
+void intsertionSort(int* ar, int size){
+    for(int i = 0; i < size - 1; i++){
+            int j = i;
+            for(j; j >= 0 && ar[j+1] < ar[j]; j--){
+                swap(ar[j + 1], ar[j]);
+            }
+
+    }
+
+
+}
+
+int getMax();
+
+int getMin();
+
+
---
+++ task.h
@@ -0,0 +1,20 @@
+#ifndef TASK_H
+#define TASK_H
+#include <iostream>
+#include <time.h>
+
+void fillRandom(int*, int);
+
+int showEl(const int*, int, int, int);
+
+void insertionSort(int*, int);
+
+void swap(int&, int&);
+
+int getMax();
+
+int getMin();
+
+
+#endif // TASK_H
+
