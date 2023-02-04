2443c5a7771752370c1b8d715b1c7ca78c4ff3ce
bachii78 <saat.aaaau78@gmail.com>
2023-02-05 00:03:46 +0300

    обновленяя

--- main.c
+++ main.c
@@ -20,9 +20,9 @@ int main(){
     for(int i = 0; i < 1000; i++){
         mas[i] = rand() % 200;
     }
-//	for(int i = 0; i < 500; i++){
-//		std::cout << mas[i] << '\t' << mas[i + 1] << '\n';
-//	}
+	for(int i = 0; i < 1000; i++){
+		std::cout << i << '\t' << mas[i + 1] << '\n';
+	}
     return 0;
 }
 
